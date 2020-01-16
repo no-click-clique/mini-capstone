ff84adb3090ee7041d56a4813ee1d05d806ff40e
dani <dani.zaghian@gmail.com>
2020-01-16 11:22:01 -0800

    adds route, controller actions, views for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,4 +5,14 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jb"
   end
 
+  def first_product_action
+    @product = Product.first #hash of product data
+    render "first_product.json.jb"
+  end
+
+  def second_product_action
+    @product = Product.find_by(id: 2)
+    render "first_product.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products_url" => "products#all_products_action"
+    get "/first_product_url" => "products#first_product_action"
+    get "/second_product_url" => "products#second_product_action"
   end
 end
