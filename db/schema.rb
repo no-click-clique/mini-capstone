c8ab7b518c10c99a5b9857439d57936a1f317378
dani <dani.zaghian@gmail.com>
2020-01-22 10:49:43 -0800

    adds seed dump gem and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_184844) do
+ActiveRecord::Schema.define(version: 2020_01_22_183444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 6, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active", default: true
+    t.integer "stock"
   end
 
 end
