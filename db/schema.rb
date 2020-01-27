9a64528c9e3833221d1608805dad416e82abdd57
dani <dani.zaghian@gmail.com>
2020-01-27 11:21:19 -0800

    adds supplier model, data, and supplier_id column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_183444) do
+ActiveRecord::Schema.define(version: 2020_01_27_190027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_01_22_183444) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true
     t.integer "stock"
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
